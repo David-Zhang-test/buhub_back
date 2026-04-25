7d54547d73c1a715238813e536f4c9bd5a00647f
david-zhang-test <1839476290@qq.com>
2026-04-24 21:50:11 -0700

    clean up: unused scripts

--- test-db.js
+++
@@ -1,7 +0,0 @@
-const { PrismaClient } = require('@prisma/client');
-const prisma = new PrismaClient();
-async function main() {
-  const fbs = await prisma.feedback.findMany();
-  console.log(fbs);
-}
-main().catch(console.error).finally(() => prisma.$disconnect());
